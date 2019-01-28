18fac626b782405606fa855ab82234369f10e43a
Felipe Ramirez <feliperamirezg@gmail.com>
2019-01-28 10:07:23 -0300

    adds simple calculator created using tdd 🎉

---
+++ VickySergioFelipe/Calculator.swift
@@ -0,0 +1,5 @@
+class Calculator{
+    func add(thisNumber numberA:Int, withThisOtherOne numberB:Int) -> Int {
+        return numberA + numberB;
+    }
+}
--- VickySergioFelipeTests/VickySergioFelipeTests.swift
+++ VickySergioFelipeTests/VickySergioFelipeTests.swift
@@ -1,34 +1,17 @@
-//
-//  VickySergioFelipeTests.swift
-//  VickySergioFelipeTests
-//
-//  Created by Felipe Ramirez on 1/25/19.
-//  Copyright © 2019 workshop. All rights reserved.
-//
-
 import XCTest
 @testable import VickySergioFelipe
 
 class VickySergioFelipeTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testHelloWorld() {
+        let numberA = 2
+        let numberB = 2
+        
+        let calculator = Calculator()
+        
+        let total = calculator.add(thisNumber: numberA, withThisOtherOne: numberB)
+        
+        XCTAssertEqual(total, 4)
     }
 
 }
